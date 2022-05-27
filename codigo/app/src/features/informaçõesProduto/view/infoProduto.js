fd4ae4d07a6cc493dbd7af0e3a48405b0c19f2e7
CaduMeints <carlos.meints@sga.pucminas.br>
2022-05-27 16:13:28 -0300

    Uptade infoProfutos.js

--- codigo/app/src/features/informaçõesProduto/view/infoProduto.js
+++ codigo/app/src/features/informaçõesProduto/view/infoProduto.js
@@ -1,3 +1,5 @@
+
+
 let nomeProduto = document.getElementById('nomeProduto')
 let precoProduto = document.getElementById('precoProduto')
 let categoriaProduto = document.getElementById('categoriaProduto')
@@ -9,12 +11,19 @@ let quantidadeProduto = document.getElementById('quantidadeProduto')
 
 
 let produto = JSON.parse(localStorage.getItem('produtos'))
-let nomeProduto1 = produto.descricao
-let precoProduto1 = produto.preco
-let categoriaProduto1 = produto.categoria
-let entregaProduto1 = produto.localEntrega
-let estadoProduto1 = produto.estadoDoProduto
+let nomeProduto1 = produto[1].descricao
+let precoProduto1 = produto[1].preco
+let categoriaProduto1 = produto[1].categoria
+let entregaProduto1 = produto[1].localEntrega
+let estadoProduto1 = produto[1].estadoDoProduto
  //let marcaProduto = produto.marca
-let quatidadeProduto1 = produto.quantidadeDoProduto
+let quatidadeProduto1 = produto[1].quantidadeDoProduto
 
 nomeProduto.innerText = nomeProduto1
+precoProduto.innerText = precoProduto1 + " R$"
+categoriaProduto.innerText = categoriaProduto1
+entregaProduto.innerText = entregaProduto1
+estadoProduto.innerText = estadoProduto1
+
+// TEMOS QUE ACHAR UM JEITO DE ATUALIZAR A QUANTIDADE RESTANTE
+quantidadeProduto.innerText = quantidadeDoProduto1 + " restantes" 
