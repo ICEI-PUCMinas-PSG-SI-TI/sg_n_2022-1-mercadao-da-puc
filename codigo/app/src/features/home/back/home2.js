446d7d4243487462c96887f6c09ac2b7b33ae941
CaduMeints <carlos.meints@sga.pucminas.br>
2022-06-02 15:02:13 -0300

    S3/ Listagem dos Produto -Finalizado

--- codigo/app/src/features/home/back/home2.js
+++ codigo/app/src/features/home/back/home2.js
@@ -18,31 +18,13 @@ function verificarLogado(){
     }
 }
 
-function listarProdutos() {
-    
-}
 window.addEventListener("load", function(event) {
-    // let telaProduto = document.createElement('div')
-    // telaProduto.classList.add('col-lg-3','col-md-6','col-sm-12','productCol')
-    // <div class="col-lg-3 col-md-6 col-sm-12 productCol">
-    //       <img src="../../res/img/notebook.jpeg" alt="" />
-    //       <p>Notebook ITAUTEC</p>
-    //       <p><strong>R$500</strong></p>
-    //     </div>
-
-    
-
-    // var usuarios = JSON.parse(localStorage.getItem('usuarios'))
-    // var caixaProdutos = []
-    // usuarios.forEach(user => {
-    //     caixaProdutos.push(user.produtos)
+    var usuarios = JSON.parse(localStorage.getItem('usuarios'))
+    usuarios.forEach(user => {
+        user.produtos.forEach(produto => {
+            let telaProduto = document.getElementById('caixaProdutos1')
+            telaProduto.innerHTML +=  `<div class="col-lg-3 col-md-6 col-sm-12 productCol"> <img src="../../res/img/notebook.jpeg" alt="" /> <p>${produto.descricao}</p> <p><strong>R$${produto.preco}</strong></p></div>`
+        })
         
-    // })
-
-
-    var caixaProdutos = JSON.parse(localStorage.getItem('produtos'))
-    caixaProdutos.forEach(produto => {
-        let telaProduto = document.getElementById('caixaProdutos1')
-        telaProduto.innerHTML +=  `<div class="col-lg-3 col-md-6 col-sm-12 productCol"> <img src="../../res/img/notebook.jpeg" alt="" /> <p>${produto.descricao}</p> <p><strong>R$${produto.preco}</strong></p></div>`
     })
   });
