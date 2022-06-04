cc824066bd53599bae4a602b0fdcdd1ff62668b8
AleksanderRibeiro <104658038+AleksanderRibeiro@users.noreply.github.com>
2022-06-04 20:19:40 -0300

    commit correções JS
    tela informações de produto funcional
    
    tela home funcional mandando para a tela de informações

--- codigo/app/src/features/informaçõesProduto/view/infoProduto.js
+++ codigo/app/src/features/informaçõesProduto/view/infoProduto.js
@@ -1,5 +1,3 @@
-
-
 let nomeProduto = document.getElementById('nomeProduto')
 let precoProduto = document.getElementById('precoProduto')
 let categoriaProduto = document.getElementById('categoriaProduto')
@@ -8,22 +6,50 @@ let estadoProduto = document.getElementById('estadoProduto')
 //let marcaProduto = document.getElementById('nomeProduto')
 let quantidadeProduto = document.getElementById('quantidadeProduto')
 
+let labelCadastre = document.getElementById('cadastre-se')
+let labelNomeLogado = document.getElementById('nome_user')
+var isLogado = false
 
+function iniciarTela() {
+  verificarLogado()
+  carregarInformacoes()
+}
 
-let produto = JSON.parse(localStorage.getItem('produtos'))
-let nomeProduto1 = produto[1].descricao
-let precoProduto1 = produto[1].preco
-let categoriaProduto1 = produto[1].categoria
-let entregaProduto1 = produto[1].localEntrega
-let estadoProduto1 = produto[1].estadoDoProduto
- //let marcaProduto = produto.marca
-let quatidadeProduto1 = produto[1].quantidadeDoProduto
+function verificarLogado() {
+  if (localStorage.getItem('usuarios') == null) {
+    alert('Usuário nao registrado ')
+  } else {
+    var usuarios = JSON.parse(localStorage.getItem('usuarios'))
+    usuarios.forEach(user => {
+      if (user.logado == true) {
+        isLogado = true
+        labelCadastre.style.display = 'none'
+        labelNomeLogado.innerHTML = user.nome
+      }
+    })
+  }
+}
 
-nomeProduto.innerText = nomeProduto1
-precoProduto.innerText = precoProduto1 + " R$"
-categoriaProduto.innerText = categoriaProduto1
-entregaProduto.innerText = entregaProduto1
-estadoProduto.innerText = estadoProduto1
+function carregarInformacoes() {
+  let idProduto = JSON.parse(localStorage.getItem('idProduto'))
+  var usuarios = JSON.parse(localStorage.getItem('usuarios'))
+  usuarios.forEach(user => {
+    user.produtos.forEach(produto => {
+      if (produto.id == idProduto) {
+        let nomeProduto1 = produto.descricao
+        let precoProduto1 = produto.preco
+        let categoriaProduto1 = produto.categoria
+        let entregaProduto1 = produto.localEntrega
+        let estadoProduto1 = produto.estadoDoProduto
+        let quatidadeProduto1 = produto.quantidadeDoProduto
 
-// TEMOS QUE ACHAR UM JEITO DE ATUALIZAR A QUANTIDADE RESTANTE
-quantidadeProduto.innerText = quantidadeDoProduto1 + " restantes" 
+        nomeProduto.innerText = nomeProduto1
+        precoProduto.innerText = precoProduto1 + ' R$'
+        categoriaProduto.innerText = categoriaProduto1
+        entregaProduto.innerText = entregaProduto1
+        estadoProduto.innerText = estadoProduto1
+        quantidadeProduto.innerText = quantidadeDoProduto1 + ' restantes'
+      }
+    })
+  })
+}
