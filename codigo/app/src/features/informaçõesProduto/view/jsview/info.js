8743f6b361d37ce3b9a9fb1dacd7f17ae68f0e89
AleksanderRibeiro <104658038+AleksanderRibeiro@users.noreply.github.com>
2022-06-17 18:48:53 -0300

    adicionando aos favos
    só não tem como tirar do favoritos

--- codigo/app/src/features/informaçõesProduto/view/jsview/info.js
+++ codigo/app/src/features/informaçõesProduto/view/jsview/info.js
@@ -2,17 +2,38 @@ let heartFav = document.getElementById('heartFav')
 let addFav = document.getElementById('addFav')
 let favoritos = document.getElementById('favoritos')
 let isFav = false
+let usuario = JSON.parse(localStorage.getItem('usuarios'))
 
 addFav.addEventListener('click', () => {
   if (isFav == false) {
     addFav.innerText = 'Remover dos favoritos'
     heartFav.classList.remove('fa-regular')
     heartFav.classList.add('fa-solid')
+    for (let i = 0; i < usuario.length; i++) {
+      if (usuario[i].logado) {
+        usuario[i].produtosFavoritos[usuario[i].produtosFavoritos.length] =
+          localStorage.getItem('idProduto')
+      }
+    }
+    localStorage.setItem('usuarios', JSON.stringify(usuario))
     isFav = true
   } else {
     addFav.innerText = 'Adicionar aos favoritos'
     heartFav.classList.remove('fa-solid')
     heartFav.classList.add('fa-regular')
+    for (let i = 0; i < usuario.length; i++) {
+      let produtoIdPagina = localStorage.getItem('idProduto')
+      if (usuario[i].logado) {
+        for (let j = 0; j < usuario[i].produtosFavoritos[j].length; j++) {
+          if (usuario[i].produtosFavoritos[j] == produtoIdPagina) {
+            console.log(j)
+            console.log(usuario[i].produtosFavoritos)
+            usuario[i].produtosFavoritos[j].splice(j, 1, 'exfav')
+          }
+        }
+      }
+    }
+    localStorage.setItem('usuarios', JSON.stringify(usuario))
     isFav = false
   }
 })
@@ -21,11 +42,29 @@ heartFav.addEventListener('click', () => {
     addFav.innerText = 'Remover dos favoritos'
     heartFav.classList.remove('fa-regular')
     heartFav.classList.add('fa-solid')
+    for (let i = 0; i < usuario.length; i++) {
+      if (usuario[i].logado) {
+        usuario[i].produtosFavoritos[usuario[i].produtosFavoritos.length] =
+          localStorage.getItem('idProduto')
+      }
+    }
+    localStorage.setItem('usuarios', JSON.stringify(usuario))
     isFav = true
   } else {
     addFav.innerText = 'Adicionar aos favoritos'
     heartFav.classList.remove('fa-solid')
     heartFav.classList.add('fa-regular')
+    for (let i = 0; i < usuario.length; i++) {
+      let produtoIdPagina = localStorage.getItem('idProduto')
+      if (usuario[i].logado) {
+        for (let j = 0; j < usuario[i].produtosFavoritos[j].length; j++) {
+          if (usuario[i].produtosFavoritos[j] == produtoIdPagina) {
+            usuario[i].produtosFavoritos[j].splice(produtoIdPagina, 1)
+          }
+        }
+      }
+    }
+    localStorage.setItem('usuarios', JSON.stringify(usuario))
     isFav = false
   }
 })
