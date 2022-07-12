2c2011f9f09e3887cfad2401400d7c2b958d01ad
CaduMeints <carlos.meints@sga.pucminas.br>
2022-07-12 15:51:51 -0300

    Update cadastroProduto.js

--- codigo/app/src/features/cadastroProduto/back/cadastroProduto.js
+++ codigo/app/src/features/cadastroProduto/back/cadastroProduto.js
@@ -10,21 +10,6 @@ const btnMobile = document.getElementById('btnMobile')
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
 
-function toggleMenu(event) {
-  if (event.type === 'touchstart') {
-    event.preventDefault()
-  }
-  const nav = document.getElementById('nav')
-  nav.classList.toggle('active')
-  const active = nav.classList.contains('active')
-  event.currentTarget.setAttribute('aria-expanded', active)
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar menu')
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir menu')
-  }
-}
-
 function verificarLogado() {
   if (localStorage.getItem('usuarios') == null) {
     alert('Usuário nao registrado ')
